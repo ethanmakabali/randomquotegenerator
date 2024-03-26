04dab6a01be55752904ee9e775e93758864dff5a
ethanmakabali <emakabali@mail.endicott.edu>
2024-03-26 13:49:45 -0400

    random quote generator
    rqg

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function generateQuote(){
+    const quotes = [
+        {
+            quote: "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
+            author: "Michael Scott, Season 5, 'The Duel'"
+        },
+        {
+            quote: "I talk a lot, so I’ve learned to tune myself out.",
+            author: "Kelly Kapoor, Season 7, 'Counseling'"
+        },
+        {
+            quote: "I’m not superstitious, but I am a little stitious.",
+            author: "Michael Scott, Season 4, 'Fun Run'"
+        }
+    ];
+
+    let arrayIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById("quotation").innerHTML = "&quot;" + quotes[arrayIndex].quote + "&quot;";
+    document.getElementById("author").innerHTML = quotes[arrayIndex].author;
+}
+
+window.onload = function () {
+    generateQuote();
+} 
+
+document.getElementById("generate").addEventListener('click', generateQuote);
